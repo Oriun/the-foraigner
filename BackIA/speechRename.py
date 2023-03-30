d1126289033f839f938ffa333ae86422f0e35712
Valentin <valentin.brehier@epitech.eu>
2023-03-30 15:44:13 +0200

    First merge with flashcard branch

--- BackIA/speechRename.py
+++
@@ -1,9 +0,0 @@
-import gtts
-from playsound import playsound
-
-def vocalResponse(text):
-    tts = gtts.gTTS(text, lang="fr")
-    # save the audio file
-    tts.save("hello.mp3")
-    # play the audio file
-    playsound("hello.mp3")
