9eda64c3a9c8004d4ed025fbf5fca049c83877c0
Lens Alex <alex.lens@etu.unice.fr>
2023-04-07 11:56:13 +0200

    [add] : accueil page style

--- next/src/pages/app/index.tsx
+++ next/src/pages/app/index.tsx
@@ -1,30 +1,82 @@
 import Link from "next/link";
 import React from "react";
+import styles from "./index.module.scss"
+import Image from 'next/image';
+
+
+//img import 
+import theforaigner from './../../assets/accueil/Group 95.png'; // Tell webpack this JS file uses this image
+import logo from './../../assets/accueil/Illustration_sans_titre 1.png'; // Tell webpack this JS file uses this image
+
+import cours from './../../assets/accueil/image 2.png'
+import exercices from './../../assets/accueil/image 3.png'
+import minijeu from './../../assets/accueil/MicrosoftTeams-image 1.png'
+import chat from './../../assets/accueil/MicrosoftTeams-image (2) 1.png'
+import reglage from './../../assets/accueil/image 4.png'
 
 const App = () => {
   return (
     <div>
-      <h1>Home page de l&apos;app</h1>
-      <h2>Design à faire</h2>
-      <br />
-      <br />
-      <br />
-      <h2>Navigation</h2>
-      <ul>
+
+      {/* Triangle */}
+      <div className={styles.triangletopright}></div>
+      <div className={styles.trianglebottomleft}></div>
+
+      <div className={styles.setting}>
+        <Link href="/app/settings">
+          <div>
+            <Image src={reglage} alt="reglage"></Image>
+          </div>
+        </Link>
+        <Link href="/app/settings">Parametres</Link>
+      </div>
+
+      <div className={styles.centerImage}>
+        <Image src={theforaigner} alt="theforaigner" />
+      </div>
+      <div className={styles.centerImage}>
+        <Image src={logo} alt="logo" />
+      </div>
+      <ul className={styles.list}>
         <li>
-          <Link href="/app/lessons">Cours</Link>
+          <Link href="/app/lessons">
+            <div className={styles.imgcircle}>
+              <Image src={cours} alt="cours"></Image>
+            </div>
+          </Link>
+          <div>
+            <Link href="/app/lessons">Cours</Link>
+          </div>
         </li>
         <li>
-          <Link href="/app/exercises">Exercices</Link>
+          <Link href="/app/exercises">
+            <div className={styles.imgcircle}>
+              <Image src={exercices} alt="exercices"></Image>
+            </div>
+          </Link>
+          <div>
+            <Link href="/app/exercises">Exercices</Link>
+          </div>
         </li>
         <li>
-          <Link href="/app/games">Mini Jeux</Link>
+          <Link href="/app/games">
+            <div className={styles.imgcircle}> 
+              <Image src={minijeu} alt="minijeu"></Image>
+            </div>
+          </Link>
+          <div>
+            <Link href="/app/games">Mini Jeux</Link>
+          </div>
         </li>
         <li>
-          <Link href="/app/chat">Chat</Link>
-        </li>
-        <li>
-          <Link href="/app/settings">Parametres</Link>
+          <Link href="/app/chat">
+            <div className={styles.imgcircle}>
+              <Image src={chat} alt="chat"></Image>
+            </div>
+          </Link>
+          <div>
+            <Link href="/app/chat">Chat</Link>
+          </div>
         </li>
       </ul>
     </div>
