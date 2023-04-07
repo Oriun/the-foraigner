d0addaffc1ced278060d055498bac25f09e2602c
Lens Alex <alex.lens@etu.unice.fr>
2023-04-07 17:13:32 +0200

    [begin] : lessons page

--- next/src/pages/app/lessons/index.tsx
+++ next/src/pages/app/lessons/index.tsx
@@ -1,17 +1,57 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "./index.module.scss"
+import Image from 'next/image';
+
+
+import theforaigner from './../../../assets/accueil/Group 95.png'; // Tell webpack this JS file uses this image
+import logo from './../../../assets/accueil/Illustration_sans_titre 1.png'; // Tell webpack this JS file uses this image
+
+import cours from './../../../assets/accueil/image 2.png'
+import reglage from './../../../assets/accueil/image 4.png'
+
 
 const Lessons = () => {
   const router = useRouter();
   return (
     <div>
-      <h1>Toutes les leçons sont visibles ici</h1>
-      <h2>Design à faire</h2>
-      <br />
-      <br />
-      <br />
-      <h2>Exemple de Navigation</h2>
+
+      {/* Triangle */}
+      <div className={styles.triangletopright}></div>
+      <div className={styles.trianglebottomleft}></div>
+
+      <div className={styles.setting}>
+        <Link href="/app/settings">
+          <div>
+            <Image src={reglage} alt="reglage"></Image>
+          </div>
+        </Link>
+        <Link href="/app/settings">Parametres</Link>
+      </div>
+
+
+      <div className={styles.headerStyle}>
+        <div className={styles.centerImage }>
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className={styles.circle}>
+            <div className={styles.imgcircle}>
+              <Image src={cours} alt="cours"></Image>
+            </div>
+          <div>
+          </div>
+        </div>
+        <div className={styles.title}>
+          <div className={styles.foraigner}>
+            <Image src={theforaigner} alt="theforaigner" />
+          </div>
+        </div>
+      </div>
+
+      <a onClick={() => router.back()}>  Retour</a>
+
+
       <ul>
         <li>
           <Link href="/app/lessons/conjugaison/7o3HF5nEoaHx29yCQrux1/past-simple-1">
@@ -20,7 +60,6 @@ const Lessons = () => {
         </li>
         <li>...</li>
         <li>
-          <a onClick={() => router.back()}>Retour</a>
         </li>
       </ul>
     </div>
